c880a68d9951d12038fa7893112e6cdf783caa49
Kim S. Skytte <kim.s.skytte@twoday.com>
2024-05-16 13:32:02 +0200

    update

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // POST endpoint to handle JSON data
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const data = req.body;
 
   // Process the data (you can add your own logic here)
-  const responseMessage = `Received JSON data: ${JSON.stringify(data)}`;
+  //const responseMessage = `Received JSON data: ${JSON.stringify(data)}`;
+
+  await generatePDF();
+
+  const responseMessage = `pdf generated successfully!`;
 
   res.status(200).send(responseMessage);
 });
@@ -23,3 +27,59 @@ app.post("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+async function generatePDF() {
+  // Load and compile the template
+
+  const templateHtml = fs.readFileSync("templates/invoice.hbs", "utf8");
+  const template = Handlebars.compile(templateHtml);
+
+  // Load data
+  const data = fs.readJsonSync("data.json", { encoding: "utf8" });
+
+  // Load CSS
+  const cssPath = path.resolve(__dirname, "css/style.css");
+  const cssContent = fs.readFileSync(cssPath, "utf8");
+
+  // Generate HTML
+  const html = template(data);
+
+  const completeHtml = `
+    <!DOCTYPE html>
+    <html lang="da">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <style>${cssContent}</style>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css'>
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'></script>
+      </head>
+      <body>
+        ${html}
+      </body>
+    </html>`;
+
+  // Launch Puppeteer and create a new page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Set the content of the page to the generated HTML
+  await page.setContent(completeHtml, {
+    waitUntil: "domcontentloaded",
+  });
+
+  //const htmlRendered = await page.content();
+  //fs.writeFileSync("out/output.html", htmlRendered);
+
+  // Create PDF from page content
+  const pdf = await page.pdf({ format: "A4" });
+
+  // Save PDF
+  fs.writeFileSync("out/output.pdf", pdf);
+
+  console.log("PDF Generated Successfully!");
+
+  await browser.close();
+}
