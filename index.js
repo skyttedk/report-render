c1d5a67134278c4ff50563c93438c7a3cf386afb
Kim S. Skytte <kim.s.skytte@twoday.com>
2024-05-18 23:57:58 +0200

    update

--- index.js
+++ index.js
@@ -72,8 +72,10 @@ async function generatePDF(layout, data) {
           <title>Document</title>
           <style>${cssContent}</style>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"></link>
           <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css'>
           <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'></script>
+          <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
         </head>
         <body>
           ${html}
